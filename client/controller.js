80ae0dbff84d4725cea811639a33314b504c6df4
Bryan Chu <bryanchu08@yahoo.com>
2012-08-31 04:32:33 -0700

    'asdf'

--- client/controller.js
+++ client/controller.js
@@ -5,95 +5,122 @@ Fractal.util = {
 			'y': y
 		}
 	},
-	getControlPts: function(ctxPt, midPt2, midPt4, endPt) {
-		function cross(v1, v2) {
-		    return v1.x * v2.y - v2.x * v1.y;
-		}
-		function getIntersect(pt1, pt2, pt3, pt4) {
-			var r = new Point(pt2.x - pt1.x, pt2.y - pt1.y);
-			var s = new Point(pt4.x - pt3.x, pt4.y - pt3.y);
-			var rCrossS = cross(r, s);
-			var t = cross(new Point(pt3.x - pt1.x, pt3.y - pt1.y)) / rCrossS;
-			return new Point(pt1.x + (r.x * t), pt1.y + (r.y * t));
-		}
-		function getSlope(pt1, pt2) {
-			return (pt2.y - pt1.y) / (pt2.x - pt1.x);
-		}
-		function getDistance(pt1, pt2) {
-			return Math.sqrt(Math.pow(pt2.x - pt1.x, 2) + Math.pow(pt2.y - pt1.y, 2));
-		}
-		function midPtIsAbove(pt1, pt2, pt3) {
-			var isAbove;
-			if (pt2.x > p1.x) {
-				if (pt3.x > pt2.x) {
-					isAbove = getSlope(pt1, pt2) > getSlope(pt2, pt3);
-				}
-				else {
-					isAbove = false;
-				}
-			}
-			else {	
-				if (pt3.x > pt2.x) {
-					isAbove = getSlope(pt1, pt2) < getSlope(pt2, pt3);
-				}
-				else {
-					isAbove = true;
-				}
-			}
-			return isAbove;
+	getEdgePoint: function(sX, sY, mX, mY, wX, wY) {
+		//check for division by 0
+		if (mX - sX == 0 || mY - sY == 0) {
+			return [sX, sY];
 		}
 
-		var midPt1 = new Point(),
-			midPt3 = new Point(),
-			midPt5 = new Point(),
-			ctrPt1 = new Point(),
-			ctrPt2 = new Point();
+		var eX1, eY1, eX2, eY2;
 
-		//calculate midpoint 1
-		var heightX1 = Math.random() * getDistance(ctxPt, midPt2);
-		var heightY1 = Math.random() * getDistance(ctxPt, midPt2);
-		var midAbove1 = midPtIsAbove(ctxPt, midPt2, midPt4);
-		if (getSlope(ctxPt, midPt2) > 0) {
-			heightX1 = heightX1 * midAbove1 ? -1 : 1;
-			midPt1.x = ctxPt.x + ((midPt2.x - ctxPt.x) / 2) + heightX1;
+		eY1 = mY + (mX < sX ? mX : wX - mX) * Math.abs(mY - sY) / Math.abs(mX - sX) * (sY < mY ? 1 : -1);
+		eX1 = sX < mX ? wX : 0;
 
-			heightY1 = heightY1 * midAbove1 ? 1 : -1;
-			midPt1.y = ctxPt.y + ((midPt2.y - ctxPt.y) / 2) + heightY1;
+		eX2 = mX + (mY < sY ? mY : wY - mY) * Math.abs(mX - sX) / Math.abs(mY - sY) * (sX < mX ? 1 : -1);
+		eY2 = sY < mY ? wY : 0;
+		
+		if (eY1 < 0 || eY1 > wY) {
+			return [eX2, eY2];
 		}
 		else {
-			heightX1 = heightX1 * midAbove1 ? 1 : -1;
-			midPt1.x = ctxPt.x + ((midPt2.x - ctxPt.x) / 2) + heightX1;
-
-			heightY1 = heightY1 * midAbove1 ? 1 : -1;
-			midPt1.y = ctxPt.y + ((midPt2.y - ctxPt.y) / 2) + heightY1;
+			return [eX1, eY2];
 		}
-		
-		//calculate midpoint 2
-		var heightX2 = Math.random() * getDistance(midPt4, endPt);
-		var heightY2 = Math.random() * getDistance(midPt4, endPt);
-		var midAbove2 = midPtIsAbove(endPt, midPt4, midPt2);
-		if (getSlope(endPt, midPt4) > 0) {
-			heightX2 = heightX2 * midAbove2 ? -1 : 1;
-			midPt5.x = endPt.x + ((midPt4.x - endPt.x) / 2) + heightX2;
-
-			heightY1 = heightY1 * midAbove2 ? -1 : 1;
-			midPt5.y = endPt.y + ((midPt4.y - endPt.y) / 2) + heightY2;
-		}
-		else {
-			heightX1 = heightX1 * midAbove2 ? 1 : -1;
-			midPt5.x = endPt.x + ((midPt4.x - endPt.x) / 2) + heightX2;
-
-			heightY1 = heightY1 * midAbove2 ? 1 : -1;
-			midPt5.y = endPt.y + ((midPt4.y - endPt.y) / 2) + heightY2;
-		}
-
-		//calculate midpoint 3
-		midPt3 = getIntesect(midpt1, midpt2, midpt4, midpt5);
-
-		//calculate control point 1
-		
 	}
 };
+
+Fractal.ctrl = {
+	numOfClicks: 0,
+	points: [new Fractal.util.Point(), new Fractal.util.Point(), new Fractal.util.Point(), new Fractal.util.Point()]
+	// getControlPts: function(ctxPt, midPt2, midPt4, endPt) {
+	// 	function cross(v1, v2) {
+	// 	    return v1.x * v2.y - v2.x * v1.y;
+	// 	}
+	// 	function getIntersect(pt1, pt2, pt3, pt4) {
+	// 		var r = new Point(pt2.x - pt1.x, pt2.y - pt1.y);
+	// 		var s = new Point(pt4.x - pt3.x, pt4.y - pt3.y);
+	// 		var rCrossS = cross(r, s);
+	// 		var t = cross(new Point(pt3.x - pt1.x, pt3.y - pt1.y)) / rCrossS;
+	// 		return new Point(pt1.x + (r.x * t), pt1.y + (r.y * t));
+	// 	}
+	// 	function getSlope(pt1, pt2) {
+	// 		return (pt2.y - pt1.y) / (pt2.x - pt1.x);
+	// 	}
+	// 	function getDistance(pt1, pt2) {
+	// 		return Math.sqrt(Math.pow(pt2.x - pt1.x, 2) + Math.pow(pt2.y - pt1.y, 2));
+	// 	}
+	// 	function midPtIsAbove(pt1, pt2, pt3) {
+	// 		var isAbove;
+	// 		if (pt2.x > p1.x) {
+	// 			if (pt3.x > pt2.x) {
+	// 				isAbove = getSlope(pt1, pt2) > getSlope(pt2, pt3);
+	// 			}
+	// 			else {
+	// 				isAbove = false;
+	// 			}
+	// 		}
+	// 		else {	
+	// 			if (pt3.x > pt2.x) {
+	// 				isAbove = getSlope(pt1, pt2) < getSlope(pt2, pt3);
+	// 			}
+	// 			else {
+	// 				isAbove = true;
+	// 			}
+	// 		}
+	// 		return isAbove;
+	// 	}
+
+	// 	// var midPt1 = new Point(),
+	// 	// 	midPt3 = new Point(),
+	// 	// 	midPt5 = new Point(),
+	// 	// 	ctrPt1 = new Point(),
+	// 	// 	ctrPt2 = new Point();
+
+	// 	// //calculate midpoint 1
+	// 	// var heightX1 = Math.random() * getDistance(ctxPt, midPt2);
+	// 	// var heightY1 = Math.random() * getDistance(ctxPt, midPt2);
+	// 	// var midAbove1 = midPtIsAbove(ctxPt, midPt2, midPt4);
+	// 	// if (getSlope(ctxPt, midPt2) > 0) {
+	// 	// 	heightX1 = heightX1 * midAbove1 ? -1 : 1;
+	// 	// 	midPt1.x = ctxPt.x + ((midPt2.x - ctxPt.x) / 2) + heightX1;
+
+	// 	// 	heightY1 = heightY1 * midAbove1 ? 1 : -1;
+	// 	// 	midPt1.y = ctxPt.y + ((midPt2.y - ctxPt.y) / 2) + heightY1;
+	// 	// }
+	// 	// else {
+	// 	// 	heightX1 = heightX1 * midAbove1 ? 1 : -1;
+	// 	// 	midPt1.x = ctxPt.x + ((midPt2.x - ctxPt.x) / 2) + heightX1;
+
+	// 	// 	heightY1 = heightY1 * midAbove1 ? 1 : -1;
+	// 	// 	midPt1.y = ctxPt.y + ((midPt2.y - ctxPt.y) / 2) + heightY1;
+	// 	// }
+		
+	// 	// //calculate midpoint 2
+	// 	// var heightX2 = Math.random() * getDistance(midPt4, endPt);
+	// 	// var heightY2 = Math.random() * getDistance(midPt4, endPt);
+	// 	// var midAbove2 = midPtIsAbove(endPt, midPt4, midPt2);
+	// 	// if (getSlope(endPt, midPt4) > 0) {
+	// 	// 	heightX2 = heightX2 * midAbove2 ? -1 : 1;
+	// 	// 	midPt5.x = endPt.x + ((midPt4.x - endPt.x) / 2) + heightX2;
+
+	// 	// 	heightY1 = heightY1 * midAbove2 ? -1 : 1;
+	// 	// 	midPt5.y = endPt.y + ((midPt4.y - endPt.y) / 2) + heightY2;
+	// 	// }
+	// 	// else {
+	// 	// 	heightX1 = heightX1 * midAbove2 ? 1 : -1;
+	// 	// 	midPt5.x = endPt.x + ((midPt4.x - endPt.x) / 2) + heightX2;
+
+	// 	// 	heightY1 = heightY1 * midAbove2 ? 1 : -1;
+	// 	// 	midPt5.y = endPt.y + ((midPt4.y - endPt.y) / 2) + heightY2;
+	// 	// }
+
+	// 	// //calculate midpoint 3
+	// 	// midPt3 = getIntesect(midpt1, midpt2, midpt4, midpt5);
+
+	// 	// //calculate control point 1
+		
+	// }
+};
+
 
 $(function() {
 	Fractal.init();
--- client/views.js
+++ client/views.js
@@ -1,4 +1,164 @@
 Fractal = function() {
+	function assignRandomMouseHandlers() {
+		var that = this;
+		var canvas = this.canvas;
+		//create config
+		var cfg = {
+			ctx: this.canvas.getContext('2d'),
+			midX1: Math.random() * this.canvas.width,
+			midY1: Math.random() * this.canvas.height,
+			midX2: Math.random() * this.canvas.width,
+			midY2: Math.random() * this.canvas.height
+		};
+		//trigger mousedown actions
+		canvasWrapper.addEventListener('mousedown', function(mouseEvent) {
+			//assign arc origin to mouse location
+			cfg.mouseX = mouseEvent.layerX;
+			cfg.mouseY = mouseEvent.layerY;
+			//assign random ending point for arc
+			if (!that.endOnEdge) {
+				cfg.endX = (canvas.width  / 2) + Math.random() * canvas.width  * (Math.random() > .5 ? 1 : -1);
+				cfg.endY = (canvas.height / 2) + Math.random() * canvas.height * (Math.random() > .5 ? 1 : -1);
+			}
+			//assign random ending point for arc on edge
+			else if (!that.endOnEdges) {
+				var side = Math.random() > .5 ? true : false;
+				cfg.endX = side ? (Math.random() > .5 ? 0 : canvas.width) : (canvas.width  / 2) + Math.random() * canvas.width  * (Math.random() > .5 ? 1 : -1);
+				cfg.endY = side ? (canvas.height / 2) + Math.random() * canvas.height * (Math.random() > .5 ? 1 : -1) : (Math.random() > .5 ? 0 : canvas.height);
+			}
+			else {
+				var side = Math.random() > .5 ? true : false;
+				cfg.mouseX = side ? (Math.random() > .5 ? 0 : canvas.width): (canvas.width  / 2) + Math.random() * canvas.width  * (Math.random() > .5 ? 1 : -1);
+				cfg.mouseY = side ? (canvas.height / 2) + Math.random() * canvas.height * (Math.random() > .5 ? 1 : -1) : (Math.random() > .5 ? 0 : canvas.height);
+				cfg.endX = side ? (Math.random() > .5 ? 0 : canvas.width) : (canvas.width  / 2) + Math.random() * canvas.width  * (Math.random() > .5 ? 1 : -1);
+				cfg.endY = side ? (canvas.height / 2) + Math.random() * canvas.height * (Math.random() > .5 ? 1 : -1) : (Math.random() > .5 ? 0 : canvas.height);
+			}
+		});
+		//trigger mouseup actions
+		canvasWrapper.addEventListener('mouseup', function(mouseEvent) {
+			cfg.startColor = 'white';
+			cfg.endColor   = 'rgb(22, 131, 221)';
+			createFractals(cfg);
+		});
+	}
+
+	function assignControlledMouseHandlers() {
+		function mouseHandler1(mouseEvent) {
+			var ctx = that.mouseCanvas1.getContext('2d');
+			this.ctx = ctx;
+			this.startX = startX;
+			this.startY = startY;
+			this.mouseEvent = mouseEvent;
+			this.edgePoint = Fractal.util.getEdgePoint(this.startX, this.startY, this.mouseEvent.layerX, this.mouseEvent.layerY, that.canvas.width, that.canvas.height);
+			createLongLine.apply(this);
+			Fractal.ctrl.points[0].x = this.edgePoint[0];
+			Fractal.ctrl.points[0].y = this.edgePoint[1];
+		}
+		function mouseHandler2(mouseEvent) {
+			var ctx = that.mouseCanvas2.getContext('2d');
+			this.ctx = ctx;
+			this.startX = startX;
+			this.startY = startY;
+			this.mouseEvent = mouseEvent;
+			var newPoint = Fractal.util.getEdgePoint(Fractal.ctrl.points[0].x, Fractal.ctrl.points[0].y, this.startX, this.startY, that.canvas.width, that.canvas.height);
+			createShortLine.apply(this);
+			Fractal.ctrl.points[2].x = mouseEvent.layerX;
+			Fractal.ctrl.points[2].y = mouseEvent.layerY;
+		}
+		function mouseHandler3(mouseEvent) {
+			var ctx = that.mouseCanvas3.getContext('2d');
+			this.ctx = ctx;
+			this.startX = startX;
+			this.startY = startY;
+			this.mouseEvent = mouseEvent;
+			this.edgePoint = Fractal.util.getEdgePoint(this.startX, this.startY, this.mouseEvent.layerX, this.mouseEvent.layerY, that.canvas.width, that.canvas.height);
+			createLongLine.apply(this);
+			Fractal.ctrl.points[3].x = this.edgePoint[0];
+			Fractal.ctrl.points[3].y = this.edgePoint[1];
+		}
+		function createLongLine() {
+			var ctx = this.ctx;
+			//erase the canvas
+			ctx.clearRect(0, 0, that.canvas.width, that.canvas.height);
+			//draw the new line
+			ctx.lineWidth = 1;
+			ctx.strokeStyle = 'white';
+			ctx.beginPath();
+			ctx.moveTo(this.startX, this.startY);
+			ctx.lineTo(this.edgePoint[0], this.edgePoint[1]);
+			ctx.stroke();
+		}
+		function createShortLine() {
+			var ctx = this.ctx;
+			//erase the canvas
+			ctx.clearRect(0, 0, that.canvas.width, that.canvas.height);
+			//draw the new line
+			ctx.lineWidth = 1;
+			ctx.strokeStyle = 'white';
+			ctx.beginPath();
+			ctx.moveTo(Fractal.ctrl.points[Fractal.ctrl.numOfClicks - 1].x, Fractal.ctrl.points[Fractal.ctrl.numOfClicks - 1].y);
+			ctx.lineTo(this.mouseEvent.layerX, this.mouseEvent.layerY);
+			ctx.stroke();
+		}
+		var that = this;
+		var startX;
+		var startY;
+		//trigger mousedown and corresponding movement actions
+		canvasWrapper.addEventListener('mousedown', function(mouseEvent) {
+			startX = mouseEvent.layerX;
+			startY = mouseEvent.layerY;
+			Fractal.ctrl.numOfClicks++;
+			switch (Fractal.ctrl.numOfClicks) {
+				case 1:
+					//remove the previous listener
+					canvasWrapper.removeEventListener('mousemove', mouseHandler3);
+
+					Fractal.ctrl.points[1].x = startX;
+					Fractal.ctrl.points[1].y = startY;
+					canvasWrapper.addEventListener('mousemove', mouseHandler1, false);
+					break;
+				case 2:
+					//remove the previous listener
+					canvasWrapper.removeEventListener('mousemove', mouseHandler1);
+
+					canvasWrapper.addEventListener('mousemove', mouseHandler2);
+					break;
+				case 3:
+					//remove the previous listener
+					canvasWrapper.removeEventListener('mousemove', mouseHandler2);
+
+					canvasWrapper.addEventListener('mousemove', mouseHandler3);
+					break;
+				case 4:
+					//remove the previous listener
+					canvasWrapper.removeEventListener('mousemove', mouseHandler3);
+					//erase all lines
+					that.mouseCanvas1.getContext('2d').clearRect(0, 0, that.canvas.width, that.canvas.height);
+					that.mouseCanvas2.getContext('2d').clearRect(0, 0, that.canvas.width, that.canvas.height);
+					that.mouseCanvas3.getContext('2d').clearRect(0, 0, that.canvas.width, that.canvas.height);
+					that.mouseCanvas4.getContext('2d').clearRect(0, 0, that.canvas.width, that.canvas.height);
+
+					Fractal.ctrl.numOfClicks = 0;
+					var edgePoint = Fractal.util.getEdgePoint(Fractal.ctrl.points[2].x, Fractal.ctrl.points[2].y, mouseEvent.layerX, mouseEvent.layerY, that.canvas.width, that.canvas.height);
+
+					var cfg = {
+						ctx: that.canvas.getContext('2d'),
+						mouseX: Fractal.ctrl.points[0].x,
+						mouseY: Fractal.ctrl.points[0].y,
+						midX1: Fractal.ctrl.points[1].x,
+						midX2: Fractal.ctrl.points[2].x,
+						midY1: Fractal.ctrl.points[1].y,
+						midY2: Fractal.ctrl.points[2].y,
+						endX: Fractal.ctrl.points[3].x,
+						endY: Fractal.ctrl.points[3].y,
+						startColor: 'white',
+						endColor: 'rgb(22, 131, 221)'
+					}
+					createFractals(cfg);
+					break;
+			}
+		});
+	}
 
 	function createFractals(cfg) {
 		for (var i = 0; i < Math.random() * 25; i++) {
@@ -26,7 +186,7 @@ Fractal = function() {
 			var gradient = ctx.createLinearGradient(cfg.mouseX, cfg.mouseY, cfg.endX, cfg.endY);
 			gradient.addColorStop(0, cfg.startColor);
 			// gradient.addColorStop(.5, endColor);
-			gradient.addColorStop(.9, 'rgba(0, 0, 0, 0)');
+			// gradient.addColorStop(1, 'rgba(0, 0, 0, 0)');
 			ctx.strokeStyle = gradient;
 			//draw the arc
 			ctx.beginPath();
@@ -38,51 +198,35 @@ Fractal = function() {
 
 	return {
 		init: function() {
-				var canvas        = $('#stage')[0],
-					canvasWrapper = $('#canvasWrapper')[0],
-					endOnEdge     = true,
-					endOnEdges    = true;
-
-				canvas.width  = $(window).width();
-				canvas.height = $(window).height();
-				//create config
-				var cfg = {
-					ctx: canvas.getContext('2d'),
-					midX1: Math.random() * canvas.width,
-					midY1: Math.random() * canvas.height,
-					midX2: Math.random() * canvas.width,
-					midY2: Math.random() * canvas.height
+				var that = {
+					canvas: $('#stage')[0],
+					mouseCanvas1: $('#mouse1')[0],
+					mouseCanvas2: $('#mouse2')[0],
+					mouseCanvas3: $('#mouse3')[0],
+					mouseCanvas4: $('#mouse4')[0],
+					canvasWrapper: $('#canvasWrapper')[0],
+					endOnEdge: true,
+					endOnEdges: true,
+					random: false
 				};
-				//trigger mousedown actions
-				canvasWrapper.addEventListener('mousedown', function(mouseEvent) {
-					//assign arc origin to mouse location
-					cfg.mouseX = mouseEvent.layerX;
-					cfg.mouseY = mouseEvent.layerY;
-					//assign random ending point for arc
-					if (!endOnEdge) {
-						cfg.endX = (canvas.width  / 2) + Math.random() * canvas.width  * (Math.random() > .5 ? 1 : -1);
-						cfg.endY = (canvas.height / 2) + Math.random() * canvas.height * (Math.random() > .5 ? 1 : -1);
-					}
-					//assign random ending point for arc on edge
-					else if (!endOnEdges) {
-						var side = Math.random() > .5 ? true : false;
-						cfg.endX = side ? (Math.random() > .5 ? 0 : canvas.width) : (canvas.width  / 2) + Math.random() * canvas.width  * (Math.random() > .5 ? 1 : -1);
-						cfg.endY = side ? (canvas.height / 2) + Math.random() * canvas.height * (Math.random() > .5 ? 1 : -1) : (Math.random() > .5 ? 0 : canvas.height);
-					}
-					else {
-						var side = Math.random() > .5 ? true : false;
-						cfg.mouseX = side ? (Math.random() > .5 ? 0 : canvas.width): (canvas.width  / 2) + Math.random() * canvas.width  * (Math.random() > .5 ? 1 : -1);
-						cfg.mouseY = side ? (canvas.height / 2) + Math.random() * canvas.height * (Math.random() > .5 ? 1 : -1) : (Math.random() > .5 ? 0 : canvas.height);
-						cfg.endX = side ? (Math.random() > .5 ? 0 : canvas.width) : (canvas.width  / 2) + Math.random() * canvas.width  * (Math.random() > .5 ? 1 : -1);
-						cfg.endY = side ? (canvas.height / 2) + Math.random() * canvas.height * (Math.random() > .5 ? 1 : -1) : (Math.random() > .5 ? 0 : canvas.height);
-					}
-				});
-				//trigger mouseup actions
-				canvasWrapper.addEventListener('mouseup', function(mouseEvent) {
-					cfg.startColor = 'white';
-					cfg.endColor   = 'rgb(22, 131, 221)';
-					createFractals(cfg);
-				});
+
+				that.canvas.width  = $(window).width();
+				that.canvas.height = $(window).height();
+				that.mouseCanvas1.width  = $(window).width();
+				that.mouseCanvas1.height = $(window).height();
+				that.mouseCanvas2.width  = $(window).width();
+				that.mouseCanvas2.height = $(window).height();
+				that.mouseCanvas3.width  = $(window).width();
+				that.mouseCanvas3.height = $(window).height();
+				that.mouseCanvas4.width  = $(window).width();
+				that.mouseCanvas4.height = $(window).height();
+
+				if (that.random){
+					assignRandomMouseHandlers.apply(that);
+				}
+				else {
+					assignControlledMouseHandlers.apply(that);
+				}
 		}
 	}
 }();
